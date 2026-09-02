c2d1c3375b951f65036f71ded45fb8a7cb288879
yuriche1144 <5044824@gmail.com>
2026-09-02 12:43:28 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'dieta-v25';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
